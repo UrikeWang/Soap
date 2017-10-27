fab0d48f3c66386e90d4ae6d9267a7be198e7705
yuling <takusax@gmail.com>
2017-10-27 15:39:50 +0800

    create main tabbar pages

--- Soap/AppDelegate.swift
+++ Soap/AppDelegate.swift
@@ -15,8 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
         return true
+    }
+    
+    func makeEntryController() -> UIViewController {
+        
+        
+    
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
---
+++ Soap/TabBar/TabBarItemType.swift
@@ -0,0 +1,23 @@
+//
+//  TabBarItemType.swift
+//  Soap
+//
+//  Created by yuling on 2017/10/27.
+//  Copyright © 2017年 yuling. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - TabBarItemType
+enum TabBarItemType {
+    
+    // MARK: Case
+    case selection, store, material
+}
+
+// MARK: - Title
+extension TabBarItemType {
+
+    
+    
+}
